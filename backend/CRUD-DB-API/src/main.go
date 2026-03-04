08b01842660419abd8906ef1eb2cc13205d5735a
Marius <mm1000@gmx.net>
2026-03-04 13:00:46 +0100

    fix: Fixed Type casting

--- backend/CRUD-DB-API/src/main.go
+++ backend/CRUD-DB-API/src/main.go
@@ -497,9 +497,9 @@ func vote(w http.ResponseWriter, r *http.Request) {
 	defer statusRows.Close()
 
 	type voteStatus struct {
-		VotingID   int  `json:"votingId"`
-		IsOpen     bool `json:"isOpen"`
-		LastChange int  `json:"lastChange,omitempty"`
+		VotingID   int        `json:"votingId"`
+		IsOpen     bool       `json:"isOpen"`
+		LastChange *time.Time `json:"lastChange,omitempty"`
 	}
 
 	for statusRows.Next() {
