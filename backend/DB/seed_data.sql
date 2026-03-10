5955ce02e938e086764f516ac63ea1166cb0cb4a
Marius <mm1000@gmx.net>
2026-03-10 08:48:33 +0100

    feat: Updated Seed Data

--- backend/DB/seed_data.sql
+++ backend/DB/seed_data.sql
@@ -8,17 +8,17 @@ INSERT INTO Land (ID, Name, POT) VALUES
     ('ESP', 'Spain', 6);
 
 INSERT INTO Komponist (Vorname, Name) VALUES
-    ('Lena', 'Meyer-Landrut'),
+    ('"Nena"', 'Gabrielle Susanne Kerner'),
     ('Thomas', 'G:son'),
     ('Aria', 'Vidal');
 
 INSERT INTO Kuenstler (Vorname, Name, Typ, Land_ID) VALUES
-    ('Lena', 'Meyer-Landrut', 'solo', 'DEU'),
+    ('"Nena"', 'Gabrielle Susanne Kerner', 'solo', 'DEU'),
     ('Alice', 'Lindgren', 'duo', 'SWE'),
     ('Jean', 'Dupont', 'solo', 'FRA');
 
 INSERT INTO Song (ID, Name, Land_ID, Kuenstler_ID, PublikumsPunkte, JuryPunkte, YoutubeURL) VALUES
-    (1, 'Satellite Reprise', 'DEU', 1, 120, 105, 'https://www.youtube.com/embed/Pfo-8z86x80'),
+    (1, 'Irgendwie, Irgendwo, Irgendwann', 'DEU', 1, 120, 105, 'https://www.youtube.com/embed/oMHLkcc9I9c'),
     (2, 'Northern Lights', 'SWE', 2, 110, 118, 'https://www.youtube.com/embed/Pfo-8z86x80'),
     (3, 'Parisian Nights', 'FRA', 3, 98, 112, NULL);
 
