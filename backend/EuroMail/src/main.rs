04d90c725458e7b7a06f5f8aa0ad3254771d2c98
Marius <mm1000@gmx.net>
2026-04-13 22:37:24 +0200

    fix: Fixed CI Errors

--- backend/EuroMail/src/main.rs
+++ backend/EuroMail/src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<()> {
     let resend = Resend::new(&resend_api_key);
     let from = "Skill-Issue@escvoting.dev";
     let subject = "Wir sind von einem IT Unternehmen und würden sie gerne nicht einstellen";
-    let to = ["Rodimemboyrazli@gmail.com"];
+    let to = ["mm1000@gmx.net"];
 
     let email = CreateEmailBaseOptions::new(from, to, subject)
         .with_html("<strong>Sehr geerhter Herr Grambow, <br> Wir würden ihnen gerne im Vorhinein absagen<strong>");
