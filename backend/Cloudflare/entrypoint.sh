a063c2829a1d4f36d796eb8674b9cd2de0074115
Marius <mm1000@gmx.net>
2026-04-23 14:51:34 +0200

    fix: fixed entrypoint

--- backend/Cloudflare/entrypoint.sh
+++ backend/Cloudflare/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Write the credentials file from environment variable
-echo "$CLOUDFLARE_TUNNEL_TOKEN" > /etc/cloudflared/cred.json
+echo "$CLOUDFLARE_SECRET" > /etc/cloudflared/cred.json
 
 # Run cloudflared tunnel
 exec cloudflared tunnel --config /etc/cloudflared/config.yml run "$CLOUDFLARE_TUNNEL_NAME"
